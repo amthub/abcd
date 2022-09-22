c4f3fd6b4efb757bbf631f3bd4cf972d88ebc47a
amthub <110405790+amthub@users.noreply.github.com>
2022-09-23 02:08:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const card = document.getElementById("ab");
+const p = document.querySelector(".p");
+const btn = document.querySelector(".btn");
+const cv = document.querySelector(".cv");
+const cvc = document.querySelector(".cvc");
+const mm = document.querySelector(".mm");
+const yy = document.querySelector(".yy");
+const dy = document.querySelector(".dy");
+const num = document.querySelector(".num");
+const numb = document.querySelector(".numb");
+const third = document.querySelector(".third");
+
+
+btn.addEventListener('click', (e) =>
+{
+   dy.innerText = ` ${mm.value}/${yy.value}`;
+   p.innerText = card.value;
+   cvc.innerText = cv.value;
+   num.innerText = numb.value;
+   // if(mm.value==="")
+   // {
+   //    const li = document.createElement('p');
+   //    li.innerText = 'Empty';
+   //    third.append(li);
+
+   // }
+})
+
+
+
